803fac5bee2db02af9514aafa079a89d97212b29
JogeshGhadai <jogeshg@mindfiresolutions.com>
2020-10-22 21:42:29 +0530

    Trying random things in templates

--- sampleproject/hello_world/views.py
+++ sampleproject/hello_world/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def hello_world(request, name):
     # import pdb;pdb.set_trace()
     print("Hello "+name)
-    return render(request, 'hello_world.html', {"name":name})
+    return render(request, 'hello_world.html', {"name":name, "chk":name[0].lower(), "itr":[1,2,3,4,5]})
