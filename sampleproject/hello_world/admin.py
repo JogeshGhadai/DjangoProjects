1082f8532352c973823ce6cd184b1024c91594ee
JogeshGhadai <jogeshg@mindfiresolutions.com>
2020-10-23 13:54:10 +0530

    Trying things in models

--- sampleproject/hello_world/admin.py
+++ sampleproject/hello_world/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from hello_world.models import Names
+
 
 # Register your models here.
+class NamesAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Names, NamesAdmin)
--- sampleproject/hello_world/views.py
+++ sampleproject/hello_world/views.py
@@ -1,6 +1,20 @@
+from hello_world.models import Names
 from django.shortcuts import render
+
 
 def hello_world(request, name):
     # import pdb;pdb.set_trace()
     print("Hello "+name)
-    return render(request, 'hello_world.html', {"name":name, "chk":name[0].lower(), "itr":[1,2,3,4,5]})
+    all_names = Names.objects.all()
+    all_name_list = [x.name for x in all_names]
+    chk_name = name.lower() in all_name_list
+    if not chk_name:
+        all_name_list.append(name)
+        name_obj = Names(name=name.lower())
+        name_obj.save()
+    context_dict = {
+            "name": name,
+            "chk": name[0].lower(),
+            "itr": list(enumerate([x.title() for x in all_name_list], start=1)),
+        }
+    return render(request, 'hello_world.html', context_dict)
